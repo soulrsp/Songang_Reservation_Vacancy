e6605cc82524ad46af5d2bc2d19e74a567b625ff
soulrsp <soulrsp@gmail.com>
2026-08-12 09:08:00 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: './', // GitHub Pages 서브디렉토리 경로 인식 필수 설정
   plugins: [react()],
   server: {
     port: 5173,
